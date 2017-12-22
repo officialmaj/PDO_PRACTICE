abef4cc558b1f0f6bfb78fcb0844302f47ae6f2d
officialmaj <sa646@njit.edu>
2017-12-22 13:28:52 -0500

    Creating HTML Table to use results: Final commit!

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
-$servername = "sql1.njit.edu";
-$username = "sa646"
-$password = "sweeney53";
+$servername = "localhost";
+$username = "root";
+$password = "root";
 $dbname = "yz746";
 
 try {
@@ -14,7 +13,40 @@ try {
     $stmt = $conn->query($sql);
     echo "Number of Records: ". $stmt->rowCount() .'<br /><br />';
     $result = $stmt->setFetchMode(PDO::FETCH_NUM);
-
 }
-
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage().'<br />';
+    die;
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>email</th>
+       <!-- <th>fname</th>
+        <th>lname</th>
+        <th>phone</th>
+        <th>birthday</th>
+        <th>gender</th>
+        <th>password</th>-->
+    </tr>
+    <?php while ($row = $stmt->fetch()) {?>
+        <tr>
+            <td><?php echo $row[0]; ?></td>
+            <td><?php echo $row[1]; ?></td>
+            <!--<td><?php /*echo $row[2]; */?></td>
+            <td><?php /*echo $row[3]; */?></td>
+            <td><?php /*echo $row[4]; */?></td>
+            <td><?php /*echo $row[5]; */?></td>
+            <td><?php /*echo $row[6]; */?></td>
+            <td><?php /*echo $row[7]; */?></td>-->
+        </tr>
+    <?php } ?>
+</table>
+</body>
+</html>
